94e2538100d80fe126528e5cc2d7f53a6b728aa8
wilzeviolin <93179639+wilzeviolin@users.noreply.github.com>
2025-02-16 04:03:26 +0800

    Update med_imageClassification.py

--- med_imageClassification.py
+++ med_imageClassification.py
@@ -183,8 +183,8 @@ def main():
             st.write(f"### **Confidence:** {confidence:.2%}")
             
             # Display warning for low confidence
-            if confidence < 0.8:
-                st.warning("⚠️ **Low Confidence Prediction:** This prediction has less than 80% confidence. Please consult a healthcare professional or pharmacist to verify this medication.")
+            if 0.5 <= confidence < 0.8:
+                st.warning("‼️ **Low Confidence Prediction:** This prediction has less than 80% confidence. Please consult a healthcare professional or pharmacist to verify this medication.")
             
             # Display medicine information based on prediction accuracy
             st.markdown("---")
@@ -193,7 +193,7 @@ def main():
                 display_medicine_info(predicted_class)
             else:
                 st.error("❌ Incorrect Prediction")
-                st.warning("⚠️ **Important:** The model has misidentified this medication. Please consult a healthcare professional or pharmacist to verify any medication before use.")
+                st.warning("‼️ **Important:** The model has misidentified this medication. Please consult a healthcare professional or pharmacist to verify any medication before use.")
                 st.write("### Actual Medicine Information:")
                 display_medicine_info(actual_class)
                 st.markdown("---")
